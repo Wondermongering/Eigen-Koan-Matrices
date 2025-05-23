6d44652c9727a7a11e76ee9d8de5939ddd033634
Tomás Pellissari Pavan <tomaspellissaripavan@gmail.com>
2025-05-23 16:20:54 -0300

    Add cultural matrix suite and paths

--- __init__.py
+++ __init__.py
@@ -1,8 +1,17 @@
-from .eigen_koan_matrix import EigenKoanMatrix, DiagonalAffect
-from .adaptive_matrix import AdaptiveEigenKoanMatrix, AdaptationEnv
-from .hierarchical_identity_tests import create_hierarchical_identity_test
+"""Convenience imports for the Eigen-Koan Matrices package."""
 
-from .ekm_repository import EKMRepository
-from .adaptive_sequence import AdaptiveTestingSequence
+from eigen_koan_matrix import EigenKoanMatrix, DiagonalAffect
+from adaptive_matrix import AdaptiveEigenKoanMatrix, AdaptationEnv
+from hierarchical_identity_tests import create_hierarchical_identity_test
+from ekm_repository import EKMRepository
+from adaptive_sequence import AdaptiveTestingSequence
 
- main
+__all__ = [
+    "EigenKoanMatrix",
+    "DiagonalAffect",
+    "AdaptiveEigenKoanMatrix",
+    "AdaptationEnv",
+    "create_hierarchical_identity_test",
+    "EKMRepository",
+    "AdaptiveTestingSequence",
+]
